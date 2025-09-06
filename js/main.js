38f3d2438da6e6db15929d397625175a28df7e2c
anton240496 <47662779+anton240496@users.noreply.github.com>
2025-09-06 09:50:04 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+$(function () {
+    // меню
+    const menubtn = document.querySelector('.menu_btn');
+    const menu = document.querySelector('.menu_nav');
+    const logo_top = document.querySelector('.logo_top');
 
+    menubtn.addEventListener('click', () => {
+        menu.classList.toggle('open')
+        if (window.innerWidth > 891) {
+            logo_top.classList.toggle('logo_open')
+        }
+    })
 });
